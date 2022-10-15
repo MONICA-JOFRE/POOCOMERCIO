05588e2a1bd8f335aac1008966627acba0337620
gabrieladel <80364233+gabrieladel@users.noreply.github.com>
2022-10-15 11:11:36 -0300

    stock

---
+++ Stock.php
@@ -0,0 +1,16 @@
+<?php
+//require_once ('detalle_pedido.php');
+//require_once ('detalle_factura.php');
+
+Class Stock {
+    
+}
+/* class A implements B, C{
+        public imprimir(){
+    
+        }
+    
+        public leer(){
+    
+        }
+    } */
