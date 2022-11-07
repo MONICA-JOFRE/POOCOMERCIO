6f36dcab1fac89af5ed20f192de64998b505c049
monica jofre <monicagjofre@gmail.com>
2022-11-07 16:38:39 -0600

    cambios

--- productos/Factura.php
+++ productos/Factura.php
@@ -1,29 +1,60 @@
 <?php
-require_once('Detallefactura.php');
 require_once('autoload.php');
-
-use \personas\Cliente;
-use \personas\Usuario; 
-
-class Factura extends Detallefactura{
+class Factura{
     static protected $numero=0;
     public $fecha;
+    public $usuario;
+    public $cliente;
+    public $producto;
+    public $cantidad;
     public $total;
 
-        function __construct($fecha,$total){
-                parent :: __construct($cantidad);
+
+
+        function __construct($fecha,$usuario,$cliente,$producto,$cantidad,$total){
                 $this->id = ++self :: $numero;
                 $this->fecha= $fecha; 
+                $this->usuario= $usuario;
+                $this->cliente= $cliente;
+                $this->producto= $producto;
+                $this->cantidad= $cantidad; 
                 $this->total = $total;
             }
         function getFecha() {
             return $this->fecha;
         }
+        function getUsuario() {
+            return $this->usuario;
+        }
+        function getCliente() {
+            return $this->cliente;
+        }
+        function getProducto() {
+            return $this->producto;
+        }
+        function getCantidad() {
+            return $this->cantidad;
+            }
         function getTotal() {
             return $this->total;
         } 
         function setFecha() {
             return $this->fecha;
+        }
+        function setUsuario() {
+            return $this->usuario;
+        }
+        function setCantidad() {
+            return $this->cantidad;
+        }
+        function setCliente() {
+            return $this->cliente;
+        }
+        function setProducto() {
+            return $this->producto;
+        }
+        function setCantidad() {
+            return $this->cantidad;
         }
         function setTotal() {
             return $this->total;
--- productos/Pedido.php
+++ productos/Pedido.php
@@ -1,21 +1,32 @@
 <?php
 require_once ('autoload.php');
-
-use \personas\Proveedor;
-use \personas\Usuario;
-
-
 class Pedido {
     static protected $npedido=0;
     public $fecha;
+    public $usuario;
+    public $proveedor;
+    public $producto;
+    public $cantidad;
+    public $total;
    
-   function __construct($fecha,$producto) {
+   function __construct($fecha,$usuario,$proveedor,$producto,$cantidad,$total) {
             $this->id = ++self ::$npedido;
             $this->fecha= $fecha;
+            $this->usuario= $usuario;
+            $this->proveedor= $proveedor;
+            $this->producto= $producto;
+            $this->cantidad= $cantidad;
+            $this->total= $total;
        }
 
         function getFecha() {
             return $this->fecha;
+        }
+        function getUsuario() {
+            return $this->usuario;
+        }
+        function getProveedor() {
+            return $this->proveedor;
         }
         function setFecha() {
             return $this->fecha;
